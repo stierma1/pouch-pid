d1dbd2ef4598710df78271dda8caf549510fe305
Mark Stier <stier020@Marks-MacBook-Pro.local>
2016-10-22 09:11:44 -0500

    fixed broken handlers

--- built/db/handlers/get-attachment.js
+++ built/db/handlers/get-attachment.js
@@ -4,7 +4,7 @@ var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = [
 
 function _asyncToGenerator(fn) { return function () { var gen = fn.apply(this, arguments); return new Promise(function (resolve, reject) { function step(key, arg) { try { var info = gen[key](arg); var value = info.value; } catch (error) { reject(error); return; } if (info.done) { resolve(value); } else { return Promise.resolve(value).then(function (value) { step("next", value); }, function (err) { step("throw", err); }); } } return step("next"); }); }; }
 
-var System = require("pid-system").default;
+var System = require("pid-system");
 
 module.exports.main = function () {
   var _ref = _asyncToGenerator(regeneratorRuntime.mark(function _callee() {
